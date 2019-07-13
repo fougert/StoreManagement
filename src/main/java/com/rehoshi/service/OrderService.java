525fd23c56fcf00f9520929b9a65416baf280f15
hoshinoharu <1196756653@qq.com>
2019-07-13 11:05:47 +0800

    修改打包产品业务方法

---
+++ src/main/java/com/rehoshi/service/OrderService.java
@@ -0,0 +1,59 @@
+package com.rehoshi.service;
+
+import com.rehoshi.dto.PageData;
+import com.rehoshi.dto.RespData;
+import com.rehoshi.model.Order;
+
+/**
+ * 订单业务
+ */
+public interface OrderService {
+
+    /**
+     * 新增订单
+     * 订单状态 为待发货
+     * @param order 需要发货的订单信息
+     * @return 返回订单id
+     */
+    RespData<String> save (Order order) ;
+
+    /**
+     * 按照id 修改订单信息
+     * @param order 需要修改的订单信息
+     * @return
+     */
+    RespData<Boolean> update(Order order) ;
+
+    /**
+     * 发送订单
+     * 修改订单状态 为已发货
+     * @param id 需要发送的订单id
+     * @return
+     */
+    RespData<Boolean> send(String id) ;
+
+    /**
+     * 删除订单
+     * @param id 需要删除的订单id
+     * @return
+     */
+    RespData<Boolean> deleteById(String id) ;
+
+
+    /**
+     * 订单分页
+     * @param search 查询关键字
+     * @param pageIndex 页码
+     * @param pageSize 每页数据量
+     * @return
+     */
+    RespData<PageData<Order>> orderInPage(String search, int pageIndex, int pageSize) ;
+
+    /**
+     * 根据id 获取订单
+     * @param id 订单id
+     * @return
+     */
+    RespData<Order> getById(String id) ;
+
+}
